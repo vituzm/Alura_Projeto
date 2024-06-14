e448c2793856bb7f4373e7f7ad577a25247a53d2
vituzm <marqueszenivitor@gmail.com>
2024-06-14 18:16:28 -0300

    mudando para 50 novamente

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10; // tentar deixar sempre o mesmo numero
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
